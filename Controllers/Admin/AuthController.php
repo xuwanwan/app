e04320ac42070399afb7b5222b204a95c9fb7c5b
bbszt <bbszta@gmail.com>
2015-02-13 15:15:06 +0800

    异常

--- Controllers/Admin/AuthController.php
+++ Controllers/Admin/AuthController.php
@@ -29,41 +29,42 @@ class AuthController extends BaseController
 
         $input = Input::only(array('email', 'password'));
 
+
         try {
         	Sentry::authenticate($input, $remember);
         	return Redirect::to('/admins');
         }
-		catch (Cartalyst\Sentry\Users\LoginRequiredException $e)
+		catch (\Cartalyst\Sentry\Users\LoginRequiredException $e)
 		{
 		    echo 'Login field is required.';
 		}
-		catch (Cartalyst\Sentry\Users\PasswordRequiredException $e)
+		catch (\Cartalyst\Sentry\Users\PasswordRequiredException $e)
 		{
 		    echo 'Password field is required.';
 		}
-		catch (Cartalyst\Sentry\Users\WrongPasswordException $e)
+		catch (\Cartalyst\Sentry\Users\WrongPasswordException $e)
 		{
 		    echo 'Wrong password, try again.';
 		}
-		catch (Cartalyst\Sentry\Users\UserNotFoundException $e)
+		catch (\Cartalyst\Sentry\Users\UserNotFoundException $e)
 		{
 		    echo 'User was not found.';
 		}
-		catch (Cartalyst\Sentry\Users\UserNotActivatedException $e)
+		catch (\Cartalyst\Sentry\Users\UserNotActivatedException $e)
 		{
 		    echo 'User is not activated.';
 		}
 
 		// The following is only required if the throttling is enabled
-		catch (Cartalyst\Sentry\Throttling\UserSuspendedException $e)
+		catch (\Cartalyst\Sentry\Throttling\UserSuspendedException $e)
 		{
 		    echo 'User is suspended.';
 		}
-		catch (Cartalyst\Sentry\Throttling\UserBannedException $e)
+		catch (\Cartalyst\Sentry\Throttling\UserBannedException $e)
 		{
 		    echo 'User is banned.';
 		}        
-
+#		return Redirect::back();
 	}
 
 	public function getLogout() {
