67ea1c78b23bdd5bef46583e191ba98760f0de9c
bbszt <bbszta@gmail.com>
2015-02-24 01:27:41 +0800

    模板引入bootstrap

--- views/layouts/main.blade.php
+++ views/layouts/main.blade.php
@@ -1,16 +1,29 @@
-<!doctype html>
-<html xmlns="http://www.w3.org/1999/xhtml"
-      xmlns:og="http://ogp.me/ns#"
-      xmlns:fb="https://www.facebook.com/2008/fbml">
-    <head>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Weile</title>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    </head>
+    <title>@section('title')</title>
 
-    <body>
-    @yield('content')
-    </body>
+    @include('partials.header')
+
+</head>
+
+<body>
+
+<div class="container" >
+
+@include('partials.notifications')
+
+@section('content')
+@show
+
+@include('partials.footer')
+
+</div>
+</body>
 </html>
---
+++ views/partials/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+{{HTML::script('jquery-1.11.2/jquery-1.11.2.min.js')}}
+{{HTML::script('bootstrap-3.3.2/dist/js/bootstrap.min.js')}}
+{{HTML::script('bootstrap-3.3.2/assets/js/docs.min.js')}}
+
+<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+{{HTML::script('bootstrap-3.3.2/assets/js/ie10-viewport-bug-workaround.js')}}
